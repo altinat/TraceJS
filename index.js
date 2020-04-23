c80dd634df2199af9c96c159c08d1f8244472306
Airichan <48298065+alphabet-works@users.noreply.github.com>
2020-04-23 17:49:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ con.connect(function (err) {
 				attch = msg.content;
 			}
 			if (attch) {
-				data.channel.send(`test`).then((msg)=>{
+				const postmsg = await msg.channel.send("Finding");
 				var url = attch.url ? attch.url : attch;
 				var urlToArr = url.toLowerCase().split('.');
 				if (img_formats.indexOf(urlToArr[urlToArr.length - 1]) == -1) return;
@@ -242,7 +242,7 @@ con.connect(function (err) {
 								//	gify(`./videos/${e.mal_id}.mp4`, `./gifs/${e.mal_id}.gif`, gif_options, function (err) {
 								//		if (err) throw err;
 								if (guilds_settings[msg.guild.id][1] == "tha") {
-									msg.edit({
+									postmsg.edit({
 										//files: [new Discord.Attachment(`./gifs/${e.mal_id}.gif`, `${e.mal_id}.gif`)],
 										embed: {
 											title: `ไงล่ะ! เจอแล้ว ฉันเก่งไหม`,
@@ -282,7 +282,7 @@ con.connect(function (err) {
 											msg_authors[msg_author] = [msg.guild.id, msg.channel.id, sendedmsg.id];
 										})
 								} else {
-									msg.edit({
+									postmsg.edit({
 										//files: [new Discord.Attachment(`./gifs/${e.mal_id}.gif`, `${e.mal_id}.gif`)],
 										embed: {
 											title: `That's what you have been waiting for!`,
@@ -329,7 +329,7 @@ con.connect(function (err) {
 							})
 					})
 					//});
-				})});
+				});
 			}
 		});
 	});
