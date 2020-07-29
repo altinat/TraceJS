38dc819971bf2feedc8e3f71a0240aef11ceb105
Airichan <48298065+alphabet-works@users.noreply.github.com>
2020-07-29 17:07:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "db_host": "localhost",
-    "db_user": "root",
-    "db_pass": "",
-    "db": "tracejs",
-    "bot_token": "NzM3OTY1NDE5NDUyNjk0NTU5.XyFB2g.OwrkfTRo25umsBRrx_xK-qE9UBc"
+    "db_host": localhost,
+    "db_user": process.env.user,
+    "db_pass": process.env.pass,
+    "db": process.env.database,
+    "bot_token": process.env.TOKEN
 }
